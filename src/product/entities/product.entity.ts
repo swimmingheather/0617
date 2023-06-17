6ab39607303798532433cdfd8647b82c068cb2e8
jisu <hesther@swim.im>
2023-06-17 11:27:53 +0900

    set entity

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,19 @@
-export class Product {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { CommonEntity } from "./common.entity";
+@Entity()
+export class Product extends CommonEntity {
+
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public desc: string;
+
+  @Column()
+  public price: number;
+
+  @Column({ default: false })
+  public isSoldOut: boolean;
+
+}
